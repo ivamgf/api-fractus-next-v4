210b93c4bd114717cb1ac1873174739a05bf64a6
ivamd <programador.ivam@gmail.com>
2026-01-21 00:00:22 -0300

    new update

--- pages/api/classes/index.ts
+++ pages/api/classes/index.ts
@@ -2,41 +2,39 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import NextCors from 'nextjs-cors'
 import connect from '../../../utils/database'
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // CORS PRIMEIRO
-  await NextCors(req, res, {
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    origin: '*',
-    optionsSuccessStatus: 200,
-  })
+interface ResponseType {
+    message: string;
+}
 
-  // PRE-FLIGHT
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
+export default async (
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
+): Promise<void> => {
 
-  try {
-    if (req.method === 'GET') {
-      const { db } = await connect()
+    await NextCors(req, res, {
+        // Options
+        methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+     })
 
-      const classes = await db
-        .collection('classes')
-        .find({})
-        .toArray()
+    try {
+        const { method } = req;
 
-      res.status(200).json(classes)
-      return
+        switch (method) {
+            case 'GET':
+
+            // Access to MongoDB and Classes data
+            const { db } = await connect();
+            const response: any = await db.collection('classes').find().toArray();
+            res.status(200).json(response);
+
+            break;
+            default:
+                res.setHeader('Allow', ['GET']);
+                res.status(405).end(`Method ${method} Not Allowed!`);
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'Internal server error!' })
     }
-
-    // Método não permitido
-    res.setHeader('Allow', ['GET'])
-    res.status(405).json({ message: 'Method Not Allowed' })
-  } catch (error) {
-    console.error('API /classes error:', error)
-    res.status(500).json({ message: 'Internal server error' })
-  }
 }
