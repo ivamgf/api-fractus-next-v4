9bba5915e240e081d587270afd28127e5785d85c
ivamd <programador.ivam@gmail.com>
2026-01-21 10:59:30 -0300

    new update

--- pages/api/_middleware.ts
+++ pages/api/_middleware.ts
@@ -1,20 +1,30 @@
-import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest) {
+
+  if (req.method === 'OPTIONS') {
+    return new NextResponse(null, {
+      status: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    })
+  }
+
   const res = NextResponse.next()
 
   res.headers.set('Access-Control-Allow-Origin', '*')
-  res.headers.set('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.headers.set(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,DELETE,OPTIONS'
+  )
   res.headers.set(
     'Access-Control-Allow-Headers',
     'Content-Type, Authorization'
   )
-
-  // Preflight
-  if (req.method === 'OPTIONS') {
-    return new NextResponse(null, { status: 200, headers: res.headers })
-  }
 
   return res
 }
